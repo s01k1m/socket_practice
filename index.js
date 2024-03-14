4e7bd0d9ca067b9735615310836fb23c1b53ac27
s01k1m <sk618dev@gmail.com>
2024-03-15 04:44:48 +0900

    connection state recovery

--- index.js
+++ index.js
@@ -7,19 +7,24 @@ const app = express();
 const server = createServer(app); // Express는 appHTTP 서버에 제공할 수 있는 함수 핸들러로 초기화됩니다
 
 // (HTTP 서버) 개체를 socket.io전달하여 새 인스턴스를 초기화
-const io = new Server(server); // Node.JS HTTP 서버( 패키지) 와 통합(또는 마운트)되는 서버 
-
+const io = new Server(server, {
+  connectionStateRecovery: {}, // 임시로 저장하기 위해서 서버 측에서 활성화해야 합니다.
+});
 
 // 우리는 웹사이트 홈에 접속할 때 호출되는 경로 핸들러를 정의합니다 .
-app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-  });
+app.get("/", (req, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
 
 // server그런 다음 들어오는 소켓에 대한 이벤트를 수신 connection하고 이를 콘솔에 기록합니다.
 io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
     io.emit("chat message", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("message is disconnected");
+    socket.disconnect();
   });
 });
 
